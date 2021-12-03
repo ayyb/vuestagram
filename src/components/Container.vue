1ca765fb7e8825e51875b293461d05ee9cde9c88
ayyb <ayyb@kakao.com>
2021-12-03 11:28:05 +0900

    vuestagram 생성

---
+++ src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  <Post v-for="(item,idx) in postList" :key="idx" :postList="item" />
+</div>
+</template>
+
+<script>
+import Post from './Post.vue'
+export default {
+    name : 'Container',
+    components:{
+        Post
+    },
+    props:{
+        postList : Array
+    }
+}
+</script>
+    
+<style>
+</style>
