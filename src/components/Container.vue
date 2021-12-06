97bb4b1d7ce7aaca0cad8874a45262e01ec34b74
ayyb <ayyb@kakao.com>
2021-12-06 12:30:29 +0900

    인스타그램 필터 적용

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,93 +1,133 @@
 <template>
   <div>
-  <!-- 화면 리스트     -->
-  <div v-if="step == 0">
-      <Post  v-for="(item,idx) in postList" :key="idx" :postList="item"  />
-  </div>
-  <!-- 필터선택페이지 -->
-  <div v-if="step == 1">
-      <div class="upload-image" :style="{ backgroundImage : `url(${selectImg})` }"></div>
-        <div class="filters">
-          <div class="filter-1"></div>
-          <div class="filter-1"></div>
-          <div class="filter-1"></div>
-          <div class="filter-1"></div>
-          <div class="filter-1"></div>
-        </div>
-  </div>
-  
-
-<!-- 글작성페이지 -->
-<div v-if="step == 2">
-    <div class="upload-image" :style="{ backgroundImage : `url(${selectImg})` }"></div>
-      <div class="write">
-        <textarea class="write-box" @input="$emit('write',$event.target.value)" placeholder="write!"></textarea>
+    <!-- 화면 리스트     -->
+    <div v-if="step == 0">
+      <Post v-for="(item, idx) in postList" :key="idx" :postList="item" />
+    </div>
+    <!-- 필터선택페이지 -->
+    <div v-if="step == 1">
+      <div
+        class="upload-image"
+        :style="{ backgroundImage: `url(${selectImg})` }"
+      ></div>
+      <div class="filters">
+        <FilterBox
+          :image="selectImg"
+          :filter="item"
+          v-for="item in filters"
+          :key="item"
+        ></FilterBox>
       </div>
-</div>
-</div>
+    </div>
+
+    <!-- 글작성페이지 -->
+    <div v-if="step == 2">
+      <div
+        class="upload-image"
+        :style="{ backgroundImage: `url(${selectImg})` }"
+      ></div>
+      <div class="write">
+        <textarea
+          class="write-box"
+          @input="$emit('write', $event.target.value)"
+          placeholder="write!"
+        ></textarea>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Post from './Post.vue'
+import FilterBox from "./FilterBox.vue";
+import Post from "./Post.vue";
 export default {
-    name : 'Container',
-    components:{
-        Post
-    },
-    props:{
-        postList : Array,
-        step : Number,
-        selectImg: String
-    },
-    data(){
-        return{
-            contents:'',
-        }
-    }
-}
+  name: "Container",
+  components: {
+    Post,
+    FilterBox,
+  },
+  props: {
+    postList: Array,
+    step: Number,
+    selectImg: String,
+  },
+  data() {
+    return {
+      contents: "",
+      filters: [
+        "aden",
+        "_1977",
+        "brannan",
+        "brooklyn",
+        "clarendon",
+        "earlybird",
+        "gingham",
+        "hudson",
+        "inkwell",
+        "kelvin",
+        "lark",
+        "lofi",
+        "maven",
+        "mayfair",
+        "moon",
+        "nashville",
+        "perpetua",
+        "reyes",
+        "rise",
+        "slumber",
+        "stinson",
+        "toaster",
+        "valencia",
+        "walden",
+        "willow",
+        "xpro2",
+      ],
+    };
+  },
+};
 </script>
     
 <style>
-.upload-image{
-width: 100%;
-height: 450px;
-background: cornflowerblue;
-background-size : cover;
+.upload-image {
+  width: 100%;
+  height: 450px;
+  background: cornflowerblue;
+  background-size: cover;
 }
-.filters{
-overflow-x:scroll;
-white-space: nowrap;
+.filters {
+  overflow-x: scroll;
+  white-space: nowrap;
 }
 .filter-1 {
-width: 100px;
-height: 100px;
-background-color: cornflowerblue;
-margin: 10px 10px 10px auto;
-padding: 8px;
-display: inline-block;
-color : white;
-background-size: cover;
+  width: 100px;
+  height: 100px;
+  background-color: cornflowerblue;
+  margin: 10px 10px 10px auto;
+  padding: 8px;
+  display: inline-block;
+  color: white;
+  background-size: cover;
 }
 .filters::-webkit-scrollbar {
-height: 5px;
+  height: 5px;
 }
 .filters::-webkit-scrollbar-track {
-background: #f1f1f1; 
+  background: #f1f1f1;
 }
 .filters::-webkit-scrollbar-thumb {
-background: #888; 
-border-radius: 5px;
+  background: #888;
+  border-radius: 5px;
 }
 .filters::-webkit-scrollbar-thumb:hover {
-background: #555; 
+  background: #555;
 }
 .write-box {
-border: none;
-width: 90%;
-height: 100px;
-padding: 15px;
-margin: auto;
-display: block;
-outline: none;
+  border: none;
+  width: 90%;
+  height: 100px;
+  padding: 15px;
+  margin: auto;
+  display: block;
+  outline: none;
 }
 </style>
---
+++ src/components/FilterBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="filter-item" :class=" [filter]"  :style="{ backgroundImage: `url(${image})` }"></div> 
+</template>
+
+<script>
+export default {
+    name : 'FilterBox',
+    props:{
+        image:String,
+        filter:Array,
+    },
+    data(){
+        return{
+            isActive: true,
+        }
+    }
+}
+</script>
+
+<style>
+.filter-item {
+  width: 100px;
+  height: 100px;
+  margin: 10px 10px 10px auto;
+  padding: 8px;
+  display: inline-block;
+  color : white;
+  background-size: cover;
+  background-position : center;
+}
+</style>
